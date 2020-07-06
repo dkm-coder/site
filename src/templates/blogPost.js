317fc440a54c3dc6b0729cf0f68b60ce68a4df67
Dave Martinez <dkm.coder@gmail.com>
2020-07-06 14:44:03 +0200

    Add content from markdown files

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,10 +1,31 @@
 import React from 'react';
-// import { graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 
-const BlogPost = ({ pageContext: { pathSlug } }) => (
+const BlogPost = ({
+    data: {
+      markdownRemark: {
+        frontmatter: {
+          title,
+        },
+        html,
+      }
+    }
+}) => (
   <div>
-    {pathSlug}
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{__html: html}} />
   </div>
 );
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: {path: {eq: $pathSlug} }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
 
 export default BlogPost;
